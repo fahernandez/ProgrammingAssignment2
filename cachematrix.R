c51baf8a8822d055a4e70a9847b4bca6ad3848a7
Fabian Hernandez <fabian.hernandez@hulihealth.com>
2015-05-17 22:17:11 -0600

    Adding solution for matrix reverse caching

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,53 @@
-## Put comments here that give an overall description of what your
-## functions do
+##  Creates a special "matrix" object that can cache and 
+##  computes the inverse of the special "matrix"
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function creates a special "matrix" object that can cache its inverse
+makeCacheMatrix <- function(matrix_to_reverse = matrix()) {
+  ## Empty the cache
+  reversed_matrix_value <- NULL
+  
+  ## Define the set function
+  set <- function(reversed_matrix) {
+    matrix_to_reverse <<- reversed_matrix
+    reversed_matrix_value <<- NULL
+  }
+  ## Define the get function
+  get <- function() matrix_to_reverse
+  
+  ## Set on cache the reversed value
+  setReversedValue <- function(reversed_value) reversed_matrix_value <<- reversed_value
+  
+  ## Get the reversed value
+  getReversedValue <- function() reversed_matrix_value
+  
+  ## Return the values
+  list(set = set, get = get,
+       setReversedValue = setReversedValue,
+       getReversedValue = getReversedValue)
 }
 
 
-## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(matrix_to_reverse, ...) {
         ## Return a matrix that is the inverse of 'x'
+        reversed_value <- matrix_to_reverse$getReversedValue()
+        
+        ## If the reversed value is null, it means that the values hasn't been cached
+        if(!is.null(reversed_value)) {
+          message("getting cached data")
+          return(reversed_value)
+        }
+        
+        ## Get the matrix to reverse
+        data <- matrix_to_reverse$get()
+        
+        ## Reverse the matrix
+        reversed_value <- solve(data, ...)
+        
+        ## Cache the revered value of the matrix
+        matrix_to_reverse$setReversedValue(reversed_value)
+        
+        ## Return the reversed value
+        reversed_value
 }
